fcdae15b63095fd97f728589897acf181bde52fd
siyamulislam <siyamulislam1@gmail.com>
2024-02-17 21:30:29 +0600

    play audio

--- client/components/home-design/dua-list.tsx
+++ client/components/home-design/dua-list.tsx
@@ -2,7 +2,7 @@
 import { DuaListProps } from "@/app/type";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const DuaList = ({ dua }: DuaListProps) => {
   const searchParams = useSearchParams();
@@ -16,6 +16,61 @@ const DuaList = ({ dua }: DuaListProps) => {
       duaListRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [duaId, dua.id]);
+
+  const [audioStates, setAudioStates] = useState<{ [key: string]: { audio: HTMLAudioElement | null, isPlaying: boolean } }>({});
+
+  useEffect(() => {
+    // Check if audioStates has changed
+    const handleAudioStateChange = (duaId: string) => {
+      if (audioStates[duaId] && audioStates[duaId].isPlaying) {
+        console.log("Audio is playing");
+      } else {
+        console.log("Audio is paused");
+      }
+    };
+
+    // Loop through each duaId
+    Object.keys(audioStates).forEach((duaId) => {
+      handleAudioStateChange(duaId);
+    });
+  }, [audioStates]);
+
+
+  const handlePlayPause = (audioUrl: string, duaId: string) => {
+    // If an audio element is already present for the given duaId
+    if (audioStates[duaId] && audioStates[duaId].audio) {
+      const { audio } = audioStates[duaId];
+      if (audio) {
+        audio.pause();
+      }
+      setAudioStates({
+        ...audioStates,
+        [duaId]: { ...audioStates[duaId], audio: null, isPlaying: false },
+      });
+    } else {
+      const newAudio = new Audio(audioUrl);
+      newAudio
+        .play()
+        .then(() => {
+          setAudioStates({
+            ...audioStates,
+            [duaId]: { audio: newAudio, isPlaying: true },
+          });
+        })
+        .catch((error) => {
+          console.error("Error playing audio:", error);
+        });
+
+      newAudio.addEventListener("ended", () => {
+        setAudioStates({
+          ...audioStates,
+          [duaId]: { ...audioStates[duaId], isPlaying: false },
+        });
+      });
+    }
+  };
+
+
   return (
     <div
       className="flex flex-col gap-4 "
@@ -42,6 +97,7 @@ const DuaList = ({ dua }: DuaListProps) => {
             <span>{dua.dua_id}. </span>
             {dua.dua_name_en}
           </p>
+          
         </div>
         <div className=" flex flex-col gap-6 leading-8">
           <p className="text-lg">{dua.top_en}</p>
@@ -59,6 +115,181 @@ const DuaList = ({ dua }: DuaListProps) => {
           <p className="text-secondary_green font-semibold">Reference:</p>
           <p className="font-medium">{dua.refference_en}</p>
         </div>
+
+
+
+
+        {/* icon start */}
+        <div className="mt-10 flex justify-end gap-[310px]">
+          {dua.audio !== null ? (
+            <button 
+            onClick={() =>
+              handlePlayPause(dua.audio as string, dua.dua_id as unknown as string)
+            }
+                          >
+          <div className="bg-[#1FA45B] p-3 rounded-full">
+            {audioStates[dua.dua_id] &&
+              audioStates[dua.dua_id].isPlaying ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-player-pause-filled text-white"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path
+                  stroke="none"
+                  d="M0 0h24v24H0z"
+                  fill="none"
+                />
+                <path
+                  d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+                  strokeWidth="0"
+                  fill="currentColor"
+                />
+                <path
+                  d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+                  strokeWidth="0"
+                  fill="currentColor"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-player-play-filled text-white"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path
+                  stroke="none"
+                  d="M0 0h24v24H0z"
+                  fill="none"
+                />
+                <path
+                  d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
+                  strokeWidth="0"
+                  fill="currentColor"
+                />
+              </svg>
+            )}
+          </div>
+        </button>
+                        ) : null}
+        <div className="flex items-center gap-10">
+          <a href="" className="">
+            <Image
+              src="/images/icon/copy.svg"
+              alt="copy"
+              title="Copy"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/bookmark.svg"
+              alt="bookmark"
+              title="Bookmark"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/plan.svg"
+              alt="plan"
+              title="Memorize"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/share.svg"
+              alt="share"
+              title="Share"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/report.svg"
+              alt="report"
+              title="Report"
+              width={24}
+              height={24}
+            />
+          </a>
+        </div>
+        </div>
+
+
+
+
+
+
+
+        {/* <div className="flex items-center gap-10">
+          <a href="" className="">
+            <Image
+              src="/images/icon/copy.svg"
+              alt="copy"
+              title="Copy"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/bookmark.svg"
+              alt="bookmark"
+              title="Bookmark"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/plan.svg"
+              alt="plan"
+              title="Memorize"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/share.svg"
+              alt="share"
+              title="Share"
+              width={24}
+              height={24}
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/images/icon/report.svg"
+              alt="report"
+              title="Report"
+              width={24}
+              height={24}
+            />
+          </a>
+        </div> */}
+
+
       </div>
     </div>
   );
